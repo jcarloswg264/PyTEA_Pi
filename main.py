af1136212b33da444f23a277766c2e5b2926c740
Jose Carlos Wittmann <jcwittmann@novationits.com>
2024-11-18 21:22:33 +0100

    Configurar tamaño inicial de la ventana a 800x480 píxeles con posibilidad de redimensionar

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from kivy.config import Config
+
+# Fijar el tamaño de la ventana
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '480')
+
 from src.ventana_principal import PyTEAApp
 
 if __name__ == "__main__":
